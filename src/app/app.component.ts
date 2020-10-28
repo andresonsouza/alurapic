b2467ea8b1088b60a0f7c2480c757c08b1dce9a1
Andreson Souza <andresonjazz@gmail.com>
2020-10-28 15:23:27 -0300

    feat: use a diretiva ngfor para carregar o array de fotos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  photos = [
+
+    {
+      url: 'https://img2.icarros.com/dbimg/imgadicionalnoticia/4/91910_1',
+      description: 'sandero'
+    },
+    {
+      url: 'https://motortudo.com/wp-content/uploads/2020/03/Ficha-T%C3%A9cnica-Hyundai-hb20S-Vision-1.6-Autom%C3%A1tico-2020.jpg',
+      description: 'hb20'
+    }
+  ];
 
 }
