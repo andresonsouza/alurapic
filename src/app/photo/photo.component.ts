94be0b50a73df5cfae2013854a4f84ff6a874a52
Andreson Souza <andresonjazz@gmail.com>
2020-10-28 13:49:05 -0300

    feat: passe dados com o inbound properties

--- src/app/photo/photo.component.ts
+++ src/app/photo/photo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'ap-photo',
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
 })
 export class PhotoComponent {
 
-  description = 'Renault Sandero';
+  @Input() description = '';
 
-  url = 'https://fotos.jornaldocarro.estadao.com.br/uploads/2019/07/01065301/renault-sandero-rs-2020-1561903176830_v2_1280x960-1160x870.jpg';
+  @Input() url = '';
 }
