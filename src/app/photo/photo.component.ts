8fb767024f9b60b9dbbf8dbf36a97bbf4c192690
Andreson Souza <andresonjazz@gmail.com>
2020-10-28 12:48:14 -0300

    feat: adicione bootstrap ao projeto e crie o primeiro componente

---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ap-photo',
+  templateUrl: './photo.component.html',
+})
+export class PhotoComponent {
+
+  description = 'Renault Sandero';
+
+  url = 'https://fotos.jornaldocarro.estadao.com.br/uploads/2019/07/01065301/renault-sandero-rs-2020-1561903176830_v2_1280x960-1160x870.jpg';
+}
