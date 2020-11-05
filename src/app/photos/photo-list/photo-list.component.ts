8fee7c6afffb8cd446fa90760dfb74dd1fe25f5a
Andreson Souza <andresonjazz@gmail.com>
2020-11-05 13:23:35 -0300

    feat: adicione um botao para carregar fotos sobe demanda

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -4,6 +4,7 @@ import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'ap-photo-list',
@@ -15,9 +16,17 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [];
   filter = '';
   debounce: Subject<string> = new Subject<string>();
-  constructor(private activatedRoute: ActivatedRoute) { }
+  hasMore = true;
+  currentPage = 1;
+  userName = '';
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService
+  ) { }
 
   ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.params.userName;
     this.photos = this.activatedRoute.snapshot.data.photos;
     this.debounce
       .pipe(debounceTime(300))
@@ -25,7 +34,15 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-      this.debounce.unsubscribe();
+    this.debounce.unsubscribe();
   }
 
+  load() {
+    this.photoService
+      .listFromUserPaginated(this.userName, ++this.currentPage)
+      .subscribe(photos => {
+        this.photos = this.photos.concat(photos);
+        if (!photos.length) { this.hasMore = false; }
+      });
+  }
 }
