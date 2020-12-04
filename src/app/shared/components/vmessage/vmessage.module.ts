31a7a8b2d91fbcbdccd8046de58545f67e0ee285
Andreson Souza <andresonjazz@gmail.com>
2020-12-04 10:56:50 -0300

    fix: remova o CommonModule do vmessage

--- src/app/shared/components/vmessage/vmessage.module.ts
+++ src/app/shared/components/vmessage/vmessage.module.ts
@@ -1,9 +1,8 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { VMessageComponent } from './vmessage.component';
 
 @NgModule({
-  declarations: [VMessageComponent, CommonModule],
+  declarations: [VMessageComponent],
   exports: [VMessageComponent]
 })
 export class VMessageModule {
